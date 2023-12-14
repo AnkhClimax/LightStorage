20fbc7c24dbf3afa0cc9f9e7d723ece026dfe062
Ankh <i@qq.com>
2023-12-14 10:19:31 +0800

    2023/12/14

---
+++ src/main/java/com/example/lightstorage/LightStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.lightstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lightstorage/LightStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lightstorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LightStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
